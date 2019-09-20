31e79bafd2379e2e1419b4d4babc8e2e7ec68605
pateli21 <pateli@asmsa.org>
2019-09-19 20:59:27 -0500

    added documentation to ICS 06

--- ICS_06_TurtleTurleTurtle/Program.java
+++ ICS_06_TurtleTurleTurtle/Program.java
@@ -1,7 +1,7 @@
 /**
  * @author Isha Patel 
  * Assignment 06 Turtle 
- * 
+ * Draws a polygon with the number of sides given by the user.
  */
 import java.util.Scanner;
 
@@ -10,19 +10,26 @@ public class Program{
 	
 	public static void main(String[] args){		
 		Scanner in = new Scanner(System.in);
-		Turtle t;
-		t = new Turtle();
 		
+		/** Asks user for the number of sides and sets that input into variable sides
+		 * Variable count is used to avoid an infinite loop
+		 * calls the method
+		 */ 
 		System.out.println("How many sides do you want your polygon to have? ");
 		int sides = in.nextInt();
 		int count = sides;
+		Turtle t;
+		t = new Turtle();
 		polygon(sides, count, t);
 	}
-	
+	/** Figures out the angle measures and draws the polygon
+	 * @param sides is the number of sides (given by the user) the polygon is supposed to be
+	 * @param count is a counter to control how many times the method runs so there is a stop
+	 * @param turtle is for the turtle command to be recognized
+	 */ 
 	public static void polygon(int sides, int count, Turtle t){		
 		if (count > 0){
-			int sumInt = (sides - 2) * 180;
-			double sumEachTurn = 180 - (sumInt /sides);
+			double sumEachTurn = 360/sides;
 			t.forward(100);
 			t.left(sumEachTurn);
 			polygon(sides, count-1, t);
